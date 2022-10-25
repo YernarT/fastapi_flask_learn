54e93a6cde8fbefe8496c764836799951c0b095c
YernarT <toktaryernar@gmail.com>
2022-10-26 01:36:21 +0600

    feat: :sparkles: 完善 put, delte 请求

--- rest_service/main.py
+++ rest_service/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Body
 
 from sqlalchemy.exc import NoResultFound, IntegrityError
-from .db import doSQL, getDBSession, User
+from .db import User
+from .utils import doSQL, getDBSession, is_authenticated
 
 app = FastAPI()
 
@@ -19,7 +20,7 @@ async def users():
 
 @app.get('/users/{id}')
 async def users(id: int):
-    response_context = {}
+    response_context = {'message': ''}
 
     try:
         user = doSQL(
@@ -29,7 +30,8 @@ async def users(id: int):
     except NoResultFound:
         user = None
         response_context['isSuccess'] = False
-        
+        response_context['message'] = 'User Not Found'
+
     response_context['data'] = {
         'user': user
     }
@@ -43,35 +45,91 @@ async def users(fullname=Body(None), password=Body(None)):
         return {'isSuccess': False, 'message': 'Fullname is required', 'data': None}
     if password == None:
         return {'isSuccess': False, 'message': 'Password is required', 'data': None}
-    
+
     user = User(fullname=fullname, password=password)
-    
+
     session = getDBSession()()
     try:
         session.add(user)
         session.commit()
     except IntegrityError:
-        return {'isSuccess': False, 'message': 'Fullname is already taken', 'data': None} 
+        return {'isSuccess': False, 'message': 'Fullname is already taken', 'data': None}
 
     return {'isSuccess': True, 'message': '', 'data': {
         'user': user
     }}
 
 
-@app.put('/users')
-async def users():
+@app.put('/users/{id}')
+async def users(id: int, uid=Body(None), fullname=Body(None), password=Body(None)):
+    if uid == None:
+        return {'isSuccess': False, 'message': 'Need Login First', 'data': None}
+    else:
+        if uid != id:
+            return {'isSuccess': False, 'message': 'Can only modify own data', 'data': None}
+
+        is_login = is_authenticated(uid)
+        if not is_login:
+            return {'isSuccess': False, 'message': 'Need Login First', 'data': None}
+
+    if fullname == None:
+        return {'isSuccess': False, 'message': 'Fullname is required', 'data': None}
+    if password == None:
+        return {'isSuccess': False, 'message': 'Password is required', 'data': None}
+
+    session = getDBSession()()
+    user = session.query(User).filter_by(id=uid).one()
+    try:
+        user.fullname = fullname
+        user.password = password
+
+        session.commit()
+    except IntegrityError:
+        return {'isSuccess': False, 'message': 'Fullname is already taken', 'data': None}
+
     return {'isSuccess': True, 'message': '', 'data': {
-        'user': {}
+        'user': user
     }}
 
 
-@app.patch('/users')
-async def users():
-    return {'isSuccess': True, 'message': '', 'data': {
-        'user': {}
-    }}
+@app.delete('/users/{id}')
+async def users(id: int, uid=Body(None)):
+    if uid == None:
+        return {'isSuccess': False, 'message': 'Need Login First', 'data': None}
+    else:
+        uid = uid['uid']
+        if uid != id:
+            return {'isSuccess': False, 'message': 'Can only delete own data', 'data': None}
+
+        is_login = is_authenticated(uid)
+        if not is_login:
+            return {'isSuccess': False, 'message': 'Need Login First', 'data': None}
+
+    session = getDBSession()()
+    session.query(User).filter_by(id=uid).delete()
+    session.commit()
+
+    return {'isSuccess': True, 'message': 'Successfully deleted', 'data': None}
 
 
-@app.delete('/users')
-async def users():
-    return {'isSuccess': True, 'message': '', 'data': None}
+@app.post('/login')
+async def login(fullname=Body(None), password=Body(None)):
+    if fullname == None:
+        return {'isSuccess': False, 'message': 'Fullname is required', 'data': None}
+    if password == None:
+        return {'isSuccess': False, 'message': 'Password is required', 'data': None}
+
+    session = getDBSession()()
+    try:
+        user = session.query(User).filter_by(fullname=fullname).one()
+    except NoResultFound:
+        return {'isSuccess': False, 'message': 'Wrong fullname or password', 'data': None}
+
+    if user.password != password:
+        return {'isSuccess': False, 'message': 'Wrong fullname or password', 'data': None}
+
+    user.is_login = True
+    session.commit()
+
+    return {'isSuccess': True, 'message': 'Login successful',
+            'data': {'uid': user.id}}
