e385f052e71e7964f0be5ec3fb7637acbe9a07df
YernarT <toktaryernar@gmail.com>
2022-10-26 13:38:57 +0600

    update: :hammer: simplified code

--- front_service/main.py
+++ front_service/main.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template, request
 
-from utils import auth, serializer
+from utils.auth import get_user
+from utils.serializer import obj_to_json
 
 app = Flask(__name__)
+
+fallback_user = {
+    'is_login': False
+}
 
 
 @app.route('/')
 def index():
+    user = get_user(request)
+
+    if not user:
+        user = fallback_user
+
     context = {
-        'user': serializer.obj_to_json(auth.get_user(request))
+        'user': user,
+        'serialized_user': obj_to_json(user)
     }
 
     return render_template('index.html', context=context)
