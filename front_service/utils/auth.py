f5fdff20f6506b9237a9d7193075a8b6f6ead2af
YernarT <toktaryernar@gmail.com>
2022-10-26 13:59:27 +0600

    update: :hammer: add fallback user

--- front_service/utils/auth.py
+++ front_service/utils/auth.py
@@ -1,22 +1,23 @@
 def get_uid(request):
-    """return uid or None"""
+    """return `uid` or `None`"""
 
     return request.cookies.get('uid')
 
 
 def get_user(request):
-    """return user or None"""
+    """return `user` or `fallback_user`"""
     import requests
-
+    
+    fallback_user = { 'is_login': False }
     uid = get_uid(request)
 
     if not uid:
-        return None
+        return fallback_user
 
     response = requests.get(
         f'http://127.0.0.1:8000/users/{uid}').json()
 
     if not response.get('is_success'):
-        return None
+        return fallback_user
 
     return response['data']['user']
