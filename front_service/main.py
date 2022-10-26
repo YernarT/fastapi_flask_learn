fc288297470672a973d6d527e4febf0b748dad95
YernarT <toktaryernar@gmail.com>
2022-10-26 13:50:20 +0600

    feat: :sparkles: logout view

--- front_service/main.py
+++ front_service/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 from utils.auth import get_user
 from utils.serializer import obj_to_json
@@ -41,6 +41,28 @@ def register():
     }
 
     return render_template('register.html', context=context)
+
+
+@app.route('/logout')
+def logout():
+    user = get_user(request)
+    uid = user['id']
+
+    import requests
+
+    requests.post(
+        f'http://127.0.0.1:8000/logout/{uid}').json()
+
+    return redirect(url_for('index'))
+
+
+@app.route('/profile')
+def profile():
+    context = {
+        'user': serializer.obj_to_json(auth.get_user(request))
+    }
+
+    return render_template('profile.html', context=context)
 
 
 if __name__ == '__main__':
